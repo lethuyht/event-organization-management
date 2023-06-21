81501167dc6b51fecb16aa8f6507da80f8b65eff
HoangTran <hoangthanh.tran@team.enouvo.com>
2023-06-22 01:16:23 +0700

    remove cart item

--- src/main/client/cart/cart.service.ts
+++ src/main/client/cart/cart.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { AddItemToCartDto } from './dto';
 import { ValidateCartItem } from './command/validateCartItem.command';
 import { CartItem } from '@/db/entities/CartItem';
 import { Cart } from '@/db/entities/Cart';
 import { messageKey } from '@/i18n';
+import { User } from '@/db/entities/User';
 
 @Injectable()
 export class CartService {
@@ -40,6 +41,24 @@ export class CartService {
     );
 
     await CartItem.save(newCartItem);
+    return { message: messageKey.BASE.SUCCESSFULLY, success: true };
+  }
+
+  async removeCartItem(cartItemId: string, currentUser: User) {
+    const cart = await Cart.findOne({
+      where: { userId: currentUser.id },
+      relations: ['cartItems'],
+    });
+
+    if (
+      !cart ||
+      (cart &&
+        cart.cartItems.findIndex((cartItem) => cartItem.id === cartItemId) < 0)
+    ) {
+      throw new BadRequestException('Không tìm thấy sản phẩm trong giỏ hàng');
+    }
+
+    await CartItem.delete({ id: cartItemId });
     return { message: messageKey.BASE.SUCCESSFULLY, success: true };
   }
 }
